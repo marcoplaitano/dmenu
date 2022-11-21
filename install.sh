cbb9660b145659e722f100ff3ef7a1b0306bbfc8
Marco Plaitano <marcoplaitano23@gmail.com>
2022-11-21 22:07:08 +0100

    added xtrace

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@
 # Date:   21 Nov 2022
 # Brief:  Installation script for dmenu.
 
+# Enable xtracing (useful when I call this script from system_setup.sh).
+# In any case, tracing is redirected to this log file and won't appear on stdout
+# nor in stderr.
+LOGFILE="/var/tmp/.installation_log"
+exec {BASH_XTRACEFD}>>$LOGFILE
+PS4='$?\011 $(date +%H:%M:%S.%3N)  $BASH_SOURCE@$LINENO   \011'
+set -x
+
 
 # Install dependencies.
 sudo apt install -y gcc make build-essential libx11-dev libxinerama-dev libxft-dev
