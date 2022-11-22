2ba92afafedeb0c3ade368efc1154f93b5361d9f
Marco Plaitano <marcoplaitano23@gmail.com>
2022-11-22 11:01:08 +0100

    handling uninstall process from makefile.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 # Enable xtracing (useful when I call this script from system_setup.sh).
 # In any case, tracing is redirected to this log file and won't appear on stdout
 # nor in stderr.
-LOGFILE="/var/tmp/.installation_log"
+LOGFILE="/var/tmp/installation.log"
 exec {BASH_XTRACEFD}>>$LOGFILE
 PS4='$?\011 $(date +%H:%M:%S.%3N)  $BASH_SOURCE@$LINENO   \011'
 set -x
@@ -16,11 +16,6 @@ set -x
 
 # Install dependencies.
 sudo apt install -y gcc make build-essential libx11-dev libxinerama-dev libxft-dev
-
-# Remove previous global installation.
-sudo rm /usr/bin/dmenu
-sudo rm /usr/bin/dmenu_path
-sudo rm /usr/bin/dmenu_run
 
 # Uninstall (just for safety) and (re)install.
 sudo make uninstall
