88e9ca7f82470ebd564155bcd4448ad57847f762
Marco Plaitano <marcoplaitano23@gmail.com>
2023-01-19 21:25:40 +0100

    replaced tabs with spaces

--- config.def.h
+++ config.def.h
@@ -5,18 +5,18 @@ static int topbar = 1;     /* -b  option; if 0, dmenu appears at bottom     */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "monospace:size=10"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = {"#aaaaaa", "#1c1c1c" },
-	[SchemeSel] = {"#ffffff", "#1c1c1c" },
-	[SchemeNormHighlight] = {"#dddddd","#005555" },
-	[SchemeSelHighlight] = {"#ffffff","#008888" },
-	[SchemeOut] = {"#00ffff", "#000000" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#aaaaaa", "#1c1c1c" },
+    [SchemeSel] = {"#ffffff", "#1c1c1c" },
+    [SchemeNormHighlight] = {"#dddddd","#005555" },
+    [SchemeSelHighlight] = {"#ffffff","#008888" },
+    [SchemeOut] = {"#00ffff", "#000000" },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
